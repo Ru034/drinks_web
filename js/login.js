4c62619cf73be361e3422d50b95664e3c50a44c2
Ru <rickqaq@gmail.com>
2024-06-14 20:15:14 +0800

    完成結帳功能與購物車

---
+++ js/login.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+    $('#login-btn').click(function (event) {
+        event.preventDefault();  // 防止表單的標準提交
+
+        // 收集表單資料
+        var account = $('#account').val();
+        var password = $('#password').val();
+
+        // 進行 AJAX 請求
+        $.ajax({
+            url: '../php/login.php',  // 目標服務器的 URL
+            type: 'POST',
+            data: {
+                'account': account.toString(),
+                'password': password.toString()
+            },
+            dataType: 'json',  // 期望從服務器返回的數據類型
+            success: function (data) {
+                console.log(data);
+                if (data.success) {
+                    //document.cookie = 'loggedin=true; path=/;';
+
+                    $('#message').text('登入成功，正在跳轉...').removeClass('error').addClass('success');
+                    setTimeout(function () {
+                        window.location.href = 'home.html';
+                    }, 2000);
+                } else {
+                    $('#message').text(data.message).removeClass('success').addClass('error');
+                }
+            },
+            error: function (xhr, status, error) {
+                $('#message').text('網路或服務器錯誤').removeClass('success').addClass('error');
+                console.error('Error:', error);
+            }
+        });
+    });
+
+    $('#register-btn').click(function () {
+        window.location.href = 'register.html';
+    });
+});
---
+++ js/register.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+    $('#register-form').on('submit', function (e) {
+        e.preventDefault(); // 防止表單的正常提交
+        $.ajax({
+            url: '../php/register.php', // PHP 處理腳本的路徑
+            type: 'POST', // 請求類型
+            data: $(this).serialize(), // 序列化表單數據
+            dataType: 'json', // 設置數據類型
+            success: function (response) {
+                $('.error').remove(); // 移除之前的錯誤訊息
+                if (response.error) {
+                    $('.login-form').append('<div class="error">' + response.message + '</div>');
+                } else {
+                    // 註冊成功的處理
+                    $('.login-window').addClass('success');
+                    $('.login-form').html('<h1>註冊成功，等待跳轉頁面!</h1>');
+                    // 可以選擇重定向到登錄頁或其他操作
+                    console.log('註冊成功');
+                    setTimeout(() => {
+                        console.log('註冊成功');
+                        window.location.href = '/pages/login.html';
+                    }, 2000);
+                }
+            },
+            error: function (xhr, status, error) {
+                $('.error').remove(); // 移除之前的錯誤訊息
+                $('.login-form').append('<div class="error">發生錯誤，請稍後再試。</div>');
+                console.log("AJAX error: " + status + ' : ' + error);
+                console.log(xhr.responseText); // 如果有的話，這將顯示服務器的回應文本
+            }
+        });
+    });
+});
